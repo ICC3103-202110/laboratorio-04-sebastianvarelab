4393f6d7663d9170aeb7b91d44b089ab0270e2fa
sebastianvarelab <svarela@miuandes.cl>
2021-05-08 16:38:58 -0400

    primer commit

--- Counter.js
+++ Counter.js
@@ -1,2 +1,23 @@
 function view(counter){
+    return counter
 }
+function update(msg,counter){
+    if (msg=="+"){
+        return counter +1
+    }
+    if (msg=="-"){
+        return counter -1
+    }
+    else{
+        return counter
+    }
+}
+counter=0
+console.log ("Count")
+console.log("(+) (-)")
+console.log(" ")
+console.log("(q) for quit")
+var prompt= require ("prompt-sync")();
+var msg= prompt ("What would yo do? ") 
+console.log(update(msg,counter))
+
